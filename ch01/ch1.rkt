4feb84909e838abb3528dcc20a73050e2131978b
matriiix <matriiix@gmail.com>
2015-07-30 15:30:47 +0800

    更新到exercise1.31

--- ch01/ch1.rkt
+++ ch01/ch1.rkt
@@ -103,3 +103,82 @@
 
 
 ;;;exercise 1.25
+
+
+;;;section 1.3.1
+
+(define (cube x) (* x x x))
+
+;;(define (sum-integers a b)
+;;  (if (> a b)
+;;      0
+;;      (+ a (sum-integers (+ a 1) b))))
+
+;;(define (sum-cubes a b)
+;;  (if (> a b)
+;;      0
+;;      (+ (cube a) (sum-cubes (+ a 1) b))))
+
+;;(define (pi-sum a b)
+;;  (if (> a b)
+;;     0
+;;      (+ (/ 1.0 (* a (+ a 2))) (pi-sum (+ a 4) b))))
+
+
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+
+(define (inc n) (+ n 1))
+(define (sum-cubes a b)
+  (sum cube a inc b))
+
+
+(define (identity x) x)
+(define (sum-integers a b)
+  (sum identity a inc b))
+
+
+(define (pi-sum a b)
+  (define (pi-term x)
+    (/ 1.0 (* x (+ x 2))))
+  (define (pi-next x)
+    (+ x 4))
+  (sum pi-term a pi-next b))
+
+
+;;;exercise 1.30
+(define (sum-iter term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ (term a) result))))
+  (iter a 0))
+
+
+;;;exercise 1.31
+(define (product term a next b)
+  (if (> a b)
+      1
+      (* (term a) (product term (next a) next b))))
+
+(define (product-iter term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (* (term a) result))))
+  (iter a 1))
+
+
+(define (dec x) (- x 1))
+(define (f x)
+  (* (/ (dec x) x)
+     (/ (inc x) x)))
+(define (plus-2 x) (+ x 2.0))
+(define (pi n)  (* 4 (product f 3 plus-2 n)))
+  
+  
+    
